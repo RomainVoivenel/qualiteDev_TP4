a38da37e2b4f0e04f5a0a427e8f4d38d0784f9e2
RomainVoivenel <143810433+RomainVoivenel@users.noreply.github.com>
2024-02-22 07:52:06 +0000

    Ajout attribut et constructeur pour Salle.java

--- src/Salle.java
+++ src/Salle.java
@@ -1,3 +1,10 @@
 public class Salle{
-    
+    private String nom;
+    private int nbPlace;
+    private boolean salleOrdinateur;
+    public Salle(String nom, int nbPlace, boolean salleOrdinateur){
+        this.nom = nom;
+        this.nbPlace = nbPlace;
+        this.salleOrdinateur = salleOrdinateur;
+    }
 }
