c01aecf13d0ff54b7d3b037761a81b1fb024abca
Clément <143797192+ClementSev2@users.noreply.github.com>
2024-02-22 09:30:51 +0100

    Etudiant.java

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,13 +1,1 @@
 
-public class Etudiant{
-    private String firstName;
-    private String lastName;
-    private String adresse;
-    private int numero_telephone;
-    private String adresse_mail;
-
-    public Etudiant(String firstName, String lastName, String adresse, int numero_telephone, String adresse){
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-}
