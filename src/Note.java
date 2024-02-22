a1f124d8a0ca56c57a38d8a5689118be1f517124
sevestre <clement.sevestre24@gmail.com>
2024-02-22 11:10:00 +0100

    Note.java

--- src/Note.java
+++ src/Note.java
@@ -1,1 +1,27 @@
+import java.time.LocalDateTime;
 
+public class Note{
+    private String nomControle;
+    private Integer note;
+    private int coefficient;
+    private LocalDateTime date;
+    
+    public Note(String nomControle, Integer note, int coefficient,LocalDateTime date){
+        this.nomControle = nomControle;
+        this.note = note;
+        this.coefficient = coefficient;
+        this.date = date;
+    }
+    public String getnomControle(){
+        return nomControle;
+    }
+    public Integer getNote(){
+        return note;
+    }
+    public int getCoefficient(){
+        return coefficient;
+    }
+    public LocalDateTime getDate(){
+        return date;
+    }
+}
