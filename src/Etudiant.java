ef8616bbeb5dbc85bfc50fc25a714424d537081d
sevestre <clement.sevestre24@gmail.com>
2024-02-22 08:43:49 +0100

    oublies Etudiant

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,8 +1,9 @@
 public class Etudiant{
     private String firstName;
     private String lastName;
-}
+
     public Etudiant(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
     }
+}
