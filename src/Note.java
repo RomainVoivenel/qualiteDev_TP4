3b5ed654c5475508541c3e46bfdc54a3236cc06c
sevestre <clement.sevestre24@gmail.com>
2024-02-22 11:50:59 +0100

    nouvelle class de Note

--- src/Note.java
+++ src/Note.java
@@ -1,5 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
 import java.time.LocalDateTime;
 
 public class Note{
+    private String nomControle;
+    private Etudiant etudiant;
+    private int coefficient;
+    private LocalDateTime date;
     
+    public Note(String nomControle,Etudiant etudiant, int coefficient,LocalDateTime date){
+        this.nomControle = nomControle;
+        this.etudiant = etudiant;
+        this.coefficient = coefficient;
+        this.date = date;
+    }
+    public String getnomControle(){
+        return this.nomControle;
+    }
+    public Etudiant getEtudiant{
+        return this.etudiant;
+    }
+    public int getCoefficient(){
+        return this.coefficient;
+    }
+    public LocalDateTime getDate(){
+        return this.date;
+    }
 }
