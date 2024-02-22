3d047f722e67c6a27830e88975b75615c35eff28
Clément <143797192+ClementSev2@users.noreply.github.com>
2024-02-22 11:22:15 +0000

    Groupe

--- src/Groupe.java
+++ src/Groupe.java
@@ -1,3 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Groupe{
-    
+    private String nom;
+    private List<Etudiant> lesEtudiants;
+    private List<Cours> lesCours;
+
+    public Groupe(String nom){
+        this.nom = nom;
+        this.lesEtudiants = new ArrayList<>();
+        this.lesCours = new ArrayList<>();
+    }
+    /**
+     * Permet d'obtenir le nom du groupe
+     * @return le nom du groupe
+     */
+    public String getNom(){
+        return this.nom;
+    }
+    /**
+     * Permet d'obtenir la liste d'étudiants présents dans le groupe
+     * @return une liste d'étudiants
+     */
+    public List<Etudiant> getEtudiants(){
+        return this.lesEtudiants;
+    }
+    /**
+     * Permet d'obtenir la liste de cours pour ce groupe
+     * @return une liste de cours
+     */
+    public List<Cours> getCours(){
+        return this.lesCours;
+    }
+    /**
+     * Permet d'ajouter un étudiant au groupe
+     * @param etudiant l'étudiant a ajouter
+     */
+    public void ajouterEtudiant(Etudiant etudiant){
+        this.lesEtudiants.add(etudiant);
+    }
+    /**
+     * Permet d'ajouter un cours au groupe
+     * @param cours le cours a ajouter
+     */
+    public void ajouterCours(Cours cours){
+        this.lesCours.add(cours);
+    }
 }
