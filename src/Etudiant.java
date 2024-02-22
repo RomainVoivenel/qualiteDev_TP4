f6070b39016014042ecebe49a06a3532ec2772ae
sevestre <clement.sevestre24@gmail.com>
2024-02-22 09:07:54 +0100

    modif Etudiant.java

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,1 +1,13 @@
 
+public class Etudiant{
+    private String firstName;
+    private String lastName;
+    private String adresse;
+    private int numero_telephone;
+    private String adresse_mail;
+
+    public Etudiant(String firstName, String lastName, String adresse, int numero_telephone, String adresse){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
