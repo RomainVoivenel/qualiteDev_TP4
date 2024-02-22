b9aa65a1e80dbb656d5de65e135e35cca5cfc9f1
Clément <143797192+ClementSev2@users.noreply.github.com>
2024-02-22 08:12:55 +0000

    ajout etudiant et note

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,9 +1,16 @@
+
 public class Etudiant{
     private String firstName;
     private String lastName;
+    private String adresse;
+    private int numero_telephone;
+    private String adresse_mail;
 
-    public Etudiant(String firstName, String lastName){
+    public Etudiant(String firstName, String lastName, String adresse, int numero_telephone, String adresse_mail){
         this.firstName = firstName;
         this.lastName = lastName;
+        this.adresse = adresse;
+        this.numero_telephone = numero_telephone;
+        this.adresse_mail = adresse_mail;
     }
 }
