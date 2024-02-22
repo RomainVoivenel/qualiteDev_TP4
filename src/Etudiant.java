3fef0f06dc0fffdb915606a7c5222577f38200e0
sevestre <clement.sevestre24@gmail.com>
2024-02-22 08:42:58 +0100

    modifications Etudiant.java

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,3 +1,8 @@
 public class Etudiant{
-    
+    private String firstName;
+    private String lastName;
 }
+    public Etudiant(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
