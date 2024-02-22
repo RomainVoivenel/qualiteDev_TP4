c64296f13d0613839bb22bf50a8289a233c2b30c
sevestre <clement.sevestre24@gmail.com>
2024-02-22 11:40:36 +0100

    Etudiant approfondis

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,10 +1,45 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class Etudiant{
     private String firstName;
     private String lastName;
+    private String adresse;
+    private List<Groupe> groupe;
+    private List<Note> note;
+    private int telephone;
+    private String adresseMail;
 
-    public Etudiant(String firstName, String lastName){
+    public Etudiant(String firstName, String lastName,String adresse,Groupe groupe,Note note,int telephone,String adresseMail){
         this.firstName = firstName;
         this.lastName = lastName;
+        this.adresse = adresse;
+        this.groupe = new ArrayList<>();
+        this.note = new ArrayList<>();
+        this.telephone = telephone;
+        this.adresseMail = adresseMail;
     }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public String getAdresse(){
+        return this.adresse;
+    }
+    public List<Groupe> getGroupe(){
+        return this.groupe;
+    }
+    public List<Note> getNote(){
+        return this.note;
+    }
+    public int getTelephone(){
+        return this.telephone;
+    }
+    public String getAdresseMail(){
+        return this.adresseMail;
+    }
+
+
 }
