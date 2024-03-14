10f6451a8dcf0883ac0f5ff922a99cec001d02a7
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-03-14 09:12:55 +0100

    Modification constructeur Horaire Ajout de creerCours avec la gestion du conflit

--- src/User.java
+++ src/User.java
@@ -1,3 +1,4 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,5 +18,15 @@ public class User {
 
     public Cours getCours(){
         return this.lesCours;
+    }
+
+    public boolean creerCours(LocalDateTime startDate, LocalDateTime endDateTime, Matiere matiere, Salle salle){
+        for (Cours cours : this.lesCours){
+            if (cours.getHoraire().getStartDate() <= startDate && cours.getHoraire().getEndDate() >= endDateTime){
+                this.lesCours.add(new Cours(matiere, new Horaire(startDate, endDateTime)));
+                return true
+            }
+        }
+        return false
     }
 }
